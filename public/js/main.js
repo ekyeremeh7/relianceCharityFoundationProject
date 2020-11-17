9667243a4780b1ad78a503b9f79a91e1eb1801f7
ekyeremeh7 <ekyeremeh7@gmail.com>
2020-11-17 19:27:49 +0000

    Added some other loaders

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,25 @@
 const loader = document.querySelector('.loader');
 
-const main = document.querySelector('.main');
+//const figure = document.querySelector('figure');
 
+// const pushingPixels = document.querySelector('.loading');
+
+const main = document.querySelector('.main');
 
 function init() {
 	setTimeout(() => {
 		loader.style.opacity = 0;
 		loader.style.display = 'none';
 
-        main.style.display = 'block';
-        setTimeout(() => (main.style.opacity = 1), 50);
-       
-	}, 1500);
+		//  figure.style.opacity = 0;
+		//  figure.style.display = 'none';
+
+		// pushingPixels.style.opacity = 0;
+		// pushingPixels.style.display = 'none';
+
+		main.style.display = 'block';
+		setTimeout(() => (main.style.opacity = 1), 50);
+	}, 2000);
 }
 
 init();
